2feaef123dc748bda410a145bf9a6c54fb230e17
jadsnnr <107815566+jadsnnr@users.noreply.github.com>
2024-05-12 15:45:03 -0300

    Update exe0011.js

--- exe0011.js
+++ exe0011.js
@@ -1,10 +1,27 @@
-
+//USANDO O ".getHours/Minutes/Seconds" porém usa mais linhas de código
 var temp = new Date()
 var hora = temp.getHours()
 var min = temp.getMinutes()
 var sec = temp.getSeconds()
 
 console.log(`Agora são exatamente ${hora}:${min}:${sec}`);
+if (hora < 5) {
+    console.log('Está tarde, descanse um pouco!');
+} else if (hora < 12) {
+    console.log('Hora de acordar!');
+} else if (hora < 18) {
+    console.log('Estude um pouco!');
+}
+else {
+    console.log('Boa noite, ainda podemos estudar um pouco mais!');
+}
+
+//USANDO toLocalTimeString PARA PREENCHER DE FORMA COMPLETA
+
+var temp = new Date()
+var hora = temp.toLocaleTime()
+
+console.log(`Agora são exatamente ${hora}`);
 if (hora < 5) {
     console.log('Está tarde, descanse um pouco!');
 } else if (hora < 12) {
